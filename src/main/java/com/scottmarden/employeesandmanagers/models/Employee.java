d7cae2f5fe9bbdf32f56459c655ce797b109c839
Scott <marden.scott@gmail.com>
2017-08-14 22:13:03 -0700

    Initial commit. Basic project to practice self join in Spring Boot

---
+++ src/main/java/com/scottmarden/employeesandmanagers/models/Employee.java
@@ -0,0 +1,92 @@
+package com.scottmarden.employeesandmanagers.models;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employees")
+public class Employee {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String firstName;
+	private String lastName;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="manager_id")
+	private Manager manager;
+	private Date createdAt;
+	private Date updatedAt;
+	
+	public Employee() {
+		
+	}
+	
+	public Employee(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	@PrePersist
+	protected void onCreate() {
+		this.setCreatedAt(new Date());
+	}
+	
+	@PreUpdate
+	protected void onUpdate() {
+		this.setUpdatedAt(new Date());
+	}
+	
+	
+	
+	
+}
---
+++ src/main/java/com/scottmarden/employeesandmanagers/models/Manager.java
@@ -0,0 +1,96 @@
+package com.scottmarden.employeesandmanagers.models;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="managers")
+public class Manager {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String firstName;
+	private String lastName;
+	@OneToMany(mappedBy="managers", fetch = FetchType.LAZY)
+	private List<Employee> employees;
+	private Date createdAt;
+	private Date updatedAt;
+	
+	public Manager() {
+		
+	}
+	
+	public Manager(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public List<Employee> getEmployees() {
+		return employees;
+	}
+
+	public void setEmployees(List<Employee> employees) {
+		this.employees = employees;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
+	@PrePersist
+	protected void onCreate() {
+		this.setCreatedAt(new Date());
+	}
+	
+	@PreUpdate
+	protected void onUpdate() {
+		this.setUpdatedAt(new Date());
+	}
+	
+}
